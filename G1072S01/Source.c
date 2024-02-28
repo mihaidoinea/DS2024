4db9abba2f6b7b1f0b8ec37e2a8a615ae68d1d7f
Mihai Doinea <mihai.doinea@oracle.com>
2024-02-28 12:31:04 +0200

    G1072S01

--- G1072S01/Source.c
+++ G1072S01/Source.c
@@ -1,8 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+void insertNumber(int**, int, int*);
+
 void main()
 {
-	printf("Hello G1072!");
+	printf("Hello G1072!\n");
 	int* array = NULL;
 	int size = 0; int number = 0;
 	printf("Give a number: ");
@@ -10,9 +13,13 @@ void main()
 
 	while (number != 0)
 	{
+		insertNumber(&array, number, &size);
+
 		for (int i = 0; i < size; i++)
 			printf("%d; ", array[i]);
-		insertNumber(&array, number, &size);
+		
+		printf("\n");
+		
 		printf("Give a number: ");
 		scanf("%d", &number);
 	}
@@ -21,9 +28,16 @@ void insertNumber(int** array, int number, int* size)
 {
 	int* tmp = (int*)malloc(sizeof(int) * (*size + 1));
 	int k = 0, i = 0;
-	while (i < *size && number > *array[i])
+	while (i < *size && number > (*array)[i])
 	{
-		tmp[k++] = *array[i++];
+		tmp[k++] = (*array)[i++];
 	}
 	tmp[k++] = number;
+	while (i < *size)
+	{
+		tmp[k++] = (*array)[i++];
+	}
+	free(*array);
+	*array = tmp;
+	(*size)++;
 }
