d5a4f02c8dec68df303211fa411a461cadd7304d
Mihai Doinea <mihai.doinea@oracle.com>
2024-03-06 14:54:20 +0200

    ProjectTemplate

---
+++ ProjectTemplate/Source.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "DataUtil.h"
+#define LINE_SIZE 256
+
+void main()
+{
+	FILE* pFile = fopen("Data.txt", "r");
+	if (pFile)
+	{
+		char delimiter[] = ",";
+		char* token = NULL;
+		float income; int index = 0;
+		unsigned short reference;
+		char buffer[LINE_SIZE], name[LINE_SIZE];
+		while (fgets(buffer, sizeof(buffer), pFile))
+		{
+			token = strtok(buffer, delimiter);
+			strcpy(name, token);
+			token = strtok(NULL, delimiter);
+			income = atof(token);
+			token = strtok(NULL, delimiter);
+			reference = atoi(token);
+			Student* stud = createStudent(name, income, reference);
+			printStudent(stud);
+		}
+		//data structure operation
+	}
+}
